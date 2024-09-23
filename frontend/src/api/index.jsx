e6296247f4f63585a6173b34f7707ca5e9a3a44c
Tanishque Sharma <109088389+Tanishque003@users.noreply.github.com>
2024-09-23 19:38:51 +0530

    Update index.jsx

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
+axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: "https://task-management-system-tau-eight.vercel.app",
+  baseURL: "https://task-management-system-frontend-five.vercel.app",
 });
 export default api;
